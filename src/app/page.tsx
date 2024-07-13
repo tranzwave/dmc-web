a973c53e5bfeadb3d5b69d805cd910bc2c896e19
Yasith Fernando <yasithf@99x.io>
2024-07-13 22:35:09 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center border text-black">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        Hello Tranzwave
+      </div>
+    </main>
+  );
+}
