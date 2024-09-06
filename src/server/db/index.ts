520e4927b7590defa036774b184363384dbd654c
Yasith Fernando <yasithf@99x.io>
2024-09-06 14:23:51 +0530

    prod db config change and add dev db

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,10 +1,23 @@
-// db.ts
-const isProduction = process.env.NODE_ENV === "production";
+// db.development.ts
+import { drizzle } from 'drizzle-orm/vercel-postgres';
+import { sql } from '@vercel/postgres';
+import postgres from "postgres";
+import { env } from "~/env";
+import * as schema from "./schema";
 
-console.log(`Current DB Environment: ${process.env.NODE_ENV}`);
+/**
+ * Cache the database connection in development. This avoids creating a new connection on every HMR update.
+ */
+const globalForDb = globalThis as unknown as {
+  conn: postgres.Sql | undefined;
+};
 
-export const { db } = isProduction
-  ? require("./db.production")
-  : require("./db.development");
+// Initialize the connection for local PostgreSQL
+export const conn = globalForDb.conn ?? postgres(env.POSTGRES_URL);
+
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+
+// Initialize the database connection using local Postgres in development
+export const db = drizzle(sql, { schema, logger: true });
 
 export type DB = typeof db;
