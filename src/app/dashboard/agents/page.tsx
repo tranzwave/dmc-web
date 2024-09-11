b930e52495c86186a3f857d33895f087968b78ef
Imesha Lakshani Nanayakkara <imeshalakshani007@gmail.com>
2024-09-11 10:12:00 +0530

    db connected with agent

--- src/app/dashboard/agents/page.tsx
+++ src/app/dashboard/agents/page.tsx
@@ -1,91 +1,130 @@
-'use client';
+"use client";
+import { ColumnDef } from "@tanstack/react-table";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import { DataTable } from "~/components/bookings/home/dataTable";
 import LoadingLayout from "~/components/common/dashboardLoading";
+import DataTableDropDown from "~/components/common/dataTableDropdown";
 import TitleBar from "~/components/common/titleBar";
 import { Button } from "~/components/ui/button";
-import { agentColumns } from "~/lib/types/agent/type";
 import { getAllAgents } from "~/server/db/queries/agents";
-import { SelectAgent, SelectCountry } from "~/server/db/schemaTypes";
+import { SelectAgent } from "~/server/db/schemaTypes";
 
 export type AgentVendorData = SelectAgent & {
-    city: SelectCountry
-}
+};
 
 const AgentHome = () => {
-    
-    const pathname = usePathname();
+  const agentVendorColumns: ColumnDef<AgentVendorData>[] = [
+    {
+      header: "Name",
+      accessorFn: (row) => row.name,
+    },
+    {
+      header: "Country",
+      accessorFn: (row) => row.country,
+    },
+    {
+      header: "Agency",
+      accessorFn: (row) => row.agency,
+    },
+    {
+      header: "Contact Number",
+      accessorFn: (row) => row.primaryContactNumber,
+    },
+    {
+      header: "Primary Email",
+      accessorFn: (row) => row.email,
+    },
+    // {
+    //     header: "Trips Completed",
+    //     accessorFn: row => row.tripsCompleted
+    // },
 
-    const [data, setData] = useState<SelectAgent[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
+    {
+      accessorKey: "id",
+      header: "",
+      cell: ({ getValue, row }) => {
+        const agent = row.original;
 
+        return (
+          <DataTableDropDown
+            data={agent}
+            routeBase="/agents/"
+            onViewPath={(data) => `/dashboard/agents/${data.id}`}
+            onEditPath={(data) => `/dashboard/agents/${data.id}/edit`}
+            onDeletePath={(data) => `/dashboard/agents/${data.id}/delete`}
+          />
+        );
+      },
+    },
+  ];
+  const pathname = usePathname();
 
-        // Fetch data on mount
-        useEffect(() => {
-            async function fetchData() {
-                try {
-                    setLoading(true);
-                    const result = await getAllAgents();
-                    setData(result);
-                } catch (error) {
-                    console.error("Failed to fetch activity data:", error);
-                    setError("Failed to load data.");
-                } finally {
-                    setLoading(false);
-                }
-            }
-    
-            fetchData();
-        }, []);
+  const [data, setData] = useState<SelectAgent[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
-    
-        if (loading) {
-            return (
-              <div>
-                <div className="flex w-full flex-row justify-between gap-1">
-                  <TitleBar title="Agents" link="toAddHotel" />
-                  <div>
-                    <Link href={`${pathname}/add`}>
-                      <Button variant="primaryGreen">Add Agent</Button>
-                    </Link>
-                  </div>
-                </div>
-                  <LoadingLayout />
-              </div>
-            );
-          }
-    
-        if (error) {
-            return <div>Error: {error}</div>;
-        }
+  // Fetch data on mount
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true);
+        const result = await getAllAgents();
+        setData(result);
+      } catch (error) {
+        console.error("Failed to fetch activity data:", error);
+        setError("Failed to load data.");
+      } finally {
+        setLoading(false);
+      }
+    }
 
+    fetchData();
+  }, []);
+
+  if (loading) {
     return (
-        <div className="flex">
-            <div className="flex-1">
-                <div className="flex flex-col gap-3">
-                    <div className="flex flex-row gap-1 w-full justify-between">
-                        <TitleBar title="Agents" link="toAddBooking" />
-                        <div>
-                        <Link href={`${pathname}/add`}>
-                             <Button variant="primaryGreen">Add Agent</Button>
-                          </Link>           
-                        </div>  
-                    </div>
-                    <div className='flex flex-row gap-3 justify-center'>
-                        <div className='w-[90%]'>
-                            <DataTable
-                                columns={agentColumns}
-                                data={data}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
+      <div>
+        <div className="flex w-full flex-row justify-between gap-1">
+          <TitleBar title="Agents" link="toAddHotel" />
+          <div>
+            <Link href={`${pathname}/add`}>
+              <Button variant="primaryGreen">Add Agent</Button>
+            </Link>
+          </div>
         </div>
+        <LoadingLayout />
+      </div>
     );
-}
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div className="flex">
+      <div className="flex-1">
+        <div className="flex flex-col gap-3">
+          <div className="flex w-full flex-row justify-between gap-1">
+            <TitleBar title="Agents" link="toAddBooking" />
+            <div>
+              <Link href={`${pathname}/add`}>
+                <Button variant="primaryGreen">Add Agent</Button>
+              </Link>
+            </div>
+          </div>
+          <div className="flex flex-row justify-center gap-3">
+            <div className="w-[90%]">
+              <DataTable columns={agentVendorColumns} data={data} />
+            </div>
+            - ;' '
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AgentHome;
--- src/server/db/queries/agents/index.ts
+++ src/server/db/queries/agents/index.ts
@@ -1,8 +1,17 @@
 "use server"
 
+import { eq } from "drizzle-orm";
 import { db } from "../..";
+import { agent } from "../../schema";
 
 
 export const getAllAgents = () => {
-    return db.query.agent.findMany()
+    return db.query.agent.findMany({
+    })
+}
+
+export const getAgentVendorById = (id: string) => {
+    return db.query.agent.findFirst({
+        where: eq(agent.id, id),
+    })
 }
