a9012e864326c92a0bad10d0d5d5b710c2f4deb1
Imesha Lakshani Nanayakkara <imeshalakshani007@gmail.com>
2024-09-17 16:06:08 +0530

    DMC-18 added activity type drop down feild

--- src/components/activities/addActivity/forms/activityForm/activityForm.tsx
+++ src/components/activities/addActivity/forms/activityForm/activityForm.tsx
@@ -1,10 +1,27 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { z } from 'zod';
+import { z } from "zod";
 import { ActivityTypeDTO } from "~/app/dashboard/activities/add/context";
 import { Button } from "~/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "~/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
 import { Input } from "~/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
+import { getAllActivityTypes } from "~/server/db/queries/activities";
+import { SelectActivityType } from "~/server/db/schemaTypes";
 
 interface ActivityFormProps {
   onAddActivity: (activity: ActivityTypeDTO) => void;
@@ -27,58 +44,130 @@ const VendorActivityForm: React.FC<ActivityFormProps> = ({ onAddActivity }) => {
     },
   });
 
+  const [activityTypes, setActivityTypes] = useState<SelectActivityType[]>([]);
+  const [selectedActivityType, setSelectedActivityType] =
+    useState<SelectActivityType>();
+  const [error, setError] = useState<string>();
+  const [loading, setLoading] = useState(false);
+
   const onSubmit: SubmitHandler<ActivityTypeDTO> = (data) => {
     onAddActivity({
-        ...data,
-        typeName : data.typeName
+      ...data,
+      typeName: data.typeName,
     });
     activityForm.reset();
   };
 
+  const fetchData = async () => {
+    try {
+      // Run both requests in parallel
+      setLoading(true);
+      const [activityTpesResponse] = await Promise.all([getAllActivityTypes()]);
+
+      if (!activityTpesResponse) {
+        throw new Error("Error fetching aactivity types");
+      }
+
+      console.log("Fetched activity types:", activityTpesResponse);
+
+      setActivityTypes(activityTpesResponse);
+      setSelectedActivityType(selectedActivityType);
+
+      setLoading(false);
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError("An unknown error occurred");
+      }
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <Form {...activityForm}>
-      <form onSubmit={activityForm.handleSubmit(onSubmit)} className="space-y-6">
+      <form
+        onSubmit={activityForm.handleSubmit(onSubmit)}
+        className="space-y-6"
+      >
         <div className="grid grid-cols-3 gap-3">
-          <FormField name="name" control={activityForm.control} render={({ field }) => (
-            <FormItem>
-              <FormLabel>Activity Name</FormLabel>
-              <FormControl>
-                <Input placeholder="Enter activity name" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )} />
-          <FormField name="typeName" control={activityForm.control} render={({ field }) => (
-            <FormItem>
-              <FormLabel>Activity Type</FormLabel>
-              <FormControl>
-                <Input
-                  type="name"
-                  value={field.value ?? ""}
-                  onChange={(e) => field.onChange(e.target.value)}
-                  placeholder="Enter activity type"
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )} />
-          <FormField name="capacity" control={activityForm.control} render={({ field }) => (
-            <FormItem>
-              <FormLabel>Capacity</FormLabel>
-              <FormControl>
-                <Input
-                  type="number"
-                  value={field.value ?? ""}
-                  onChange={(e) => field.onChange(e.target.valueAsNumber)}
-                  placeholder="Enter capacity"
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )} />
+          <FormField
+            name="name"
+            control={activityForm.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Activity Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter activity name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            name="typeName"
+            // control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Activity Type</FormLabel>
+                <FormControl>
+                  <Select
+                    onValueChange={(valueFromSelection) => {
+                      field.onChange(valueFromSelection);
+                      setSelectedActivityType(
+                        activityTypes.find(
+                          (a) => a.name === valueFromSelection,
+                        ),
+                      );
+                    }}
+                    value={field.value}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger className="bg-slate-100 shadow-md">
+                      <SelectValue placeholder="Select activity type" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {activityTypes.map((type) => (
+                        <SelectItem key={type.id} value={type.name}>
+                          {type.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            name="capacity"
+            control={activityForm.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Capacity</FormLabel>
+                <FormControl>
+                  <Input
+                    type="number"
+                    value={field.value ?? ""}
+                    onChange={(e) => field.onChange(e.target.valueAsNumber)}
+                    placeholder="Enter capacity"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </div>
-        <div className="w-full flex flex-row justify-end">
-          <Button type="submit" variant={'primaryGreen'} className="px-5">Add Activity</Button>
+        <div className="flex w-full flex-row justify-end">
+          <Button type="submit" variant={"primaryGreen"} className="px-5">
+            Add Activity
+          </Button>
         </div>
       </form>
     </Form>
@@ -86,3 +175,4 @@ const VendorActivityForm: React.FC<ActivityFormProps> = ({ onAddActivity }) => {
 };
 
 export default VendorActivityForm;
+
