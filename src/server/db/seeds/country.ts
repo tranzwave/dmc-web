fe8bdd04c15fd85aa4a50a7c5b1665906b6aa119
Yasith Fernando <yasithf@99x.io>
2024-11-11 02:30:11 +0530

    add country seeds

--- src/server/db/seeds/country.ts
+++ src/server/db/seeds/country.ts
@@ -1,7 +1,14 @@
 import { DB } from "..";
 import { country } from "../schema";
 import countries from './data/countries.json'
+import { Country, State, City }  from 'country-state-city';
 
 export default async function seed(db:DB) {
-    await db.insert(country).values(countries);
+
+    await db.insert(country).values(Country.getAllCountries().map(c => {
+        return {
+            name: c.name,
+            code: c.isoCode,
+        }
+    }));
 }
