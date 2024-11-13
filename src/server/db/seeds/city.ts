d85f36b1f17ce37dfb6d285359a49783c09b6102
Yasith Fernando <yasithf@99x.io>
2024-11-13 13:52:00 +0530

    add cities

--- src/server/db/seeds/city.ts
+++ src/server/db/seeds/city.ts
@@ -2,56 +2,65 @@ import { and, eq } from "drizzle-orm";
 import { DB } from "..";
 import cities from './data/cities.json'
 import { country,city } from "../schema";
+import { Country, State, City, ICity }  from 'country-state-city';
 
 export default async function seed(db:DB) {
-    
+
+  const SriLankancCities = City.getCitiesOfCountry("LK")
+  const SeychelleCities = City.getCitiesOfCountry("SC")
+  let allCities:ICity[] = []
+
+  if(SriLankancCities && SeychelleCities){
+    allCities = [...SriLankancCities, ...SeychelleCities]
+  }
+
     // Fetch or create countries
-    const countries = await Promise.all(
-      cities.map(async (currentCity) => {
-        const foundCountry = await db.query.country.findFirst({
-          where: eq(country.code, currentCity.country),
-        });
+    // const countries = await Promise.all(
+    //   cities.map(async (currentCity) => {
+    //     const foundCountry = await db.query.country.findFirst({
+    //       where: eq(country.code, currentCity.country),
+    //     });
   
-        if (!foundCountry) {
-          // Create a new country if it does not exist
-          const newCountryCode = await db.insert(country).values(
-            {
-                "name": `${currentCity.country}-C`,
-                "code": currentCity.country
-            },
-          ).returning({
-            code: country.code
-          });
+    //     if (!foundCountry) {
+    //       // Create a new country if it does not exist
+    //       const newCountryCode = await db.insert(country).values(
+    //         {
+    //             "name": `${currentCity.country}-C`,
+    //             "code": currentCity.country
+    //         },
+    //       ).returning({
+    //         code: country.code
+    //       });
   
-          return newCountryCode[0]?.code;
-        }
+    //       return newCountryCode[0]?.code;
+    //     }
   
-        return foundCountry.code;
-      })
-    );
+    //     return foundCountry.code;
+    //   })
+    // );
 
 
     // Flatten country IDs array
-    const countryCodes = [...new Set(countries)]; // Remove duplicates
+    const countryCodes = ["LK", "SC"]; // Remove duplicates
 
     // Fetch or create cities
     await Promise.all(
-      cities.map(async (currentCity) => {
-        const countryCode = countryCodes.find(code => code === currentCity.country);
+      allCities.map(async (currentCity) => {
+        const countryCode = countryCodes.find(code => code === currentCity.countryCode);
   
         if (!countryCode) {
-          throw new Error("Country not found for city: " + currentCity.country);
+          throw new Error("Country not found for city: " + currentCity.countryCode);
         }
   
         // Check if city already exists
         const foundCity = await db.query.city.findFirst({
-          where: and(eq(city.name, currentCity.name),eq(city.country,currentCity.country)),
+          where: and(eq(city.name, currentCity.name),eq(city.country,currentCity.countryCode)),
         });
   
         if (!foundCity) {
           // Create a new city if it does not exist
           await db.insert(city).values({
-            "country" : currentCity.country,
+            "country" : currentCity.countryCode,
             "name" : currentCity.name,
           });
         }
