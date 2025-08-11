0de8d8b156a153d5aa652077993017a00726bc01
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-08-11 15:46:49 +0530

    fix: update Footer import to named import in RefundPolicy component

--- src/app/refund-policy/page.tsx
+++ src/app/refund-policy/page.tsx
@@ -1,4 +1,4 @@
-import Footer from "~/components/footer/footer";
+import {Footer} from "~/components/footer/footer";
 import { Navbar } from "~/components/navbar/navBar";
 
 export default function RefundPolicy() {
